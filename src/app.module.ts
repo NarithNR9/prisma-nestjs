d8dbfe599ad7bcacd40a6bd8f5e4384a78af1169
NarithNR9 <79370506+NarithNR9@users.noreply.github.com>
2023-03-22 08:19:13 +0700

    another commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { PostsModule } from './posts/posts.module';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, PostsModule],
+  imports: [
+    PrismaModule,
+    ArticlesModule,
+    PostsModule,
+    MulterModule.register({
+      dest: './uploads',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+  UnsupportedMediaTypeException,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('posts')
 export class PostsController {
@@ -10,6 +24,34 @@ export class PostsController {
   @Post()
   create(@Body() createPostDto: CreatePostDto) {
     return this.postsService.create(createPostDto);
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+      fileFilter: (req, file, cb) => {
+        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+          return cb(
+            new UnsupportedMediaTypeException('Wrong image file format.'),
+            false,
+          );
+        }
+        cb(null, true);
+      }
+    })
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return { data: file};
   }
 
   @Get()
